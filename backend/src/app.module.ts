f32e5ff433d08ccb0ecab4752402ced8ac4120ec
Valery.Prankevich <vprankevich@gmail.com>
2021-12-23 16:53:06 +0300

    NestJs GraphQL, Mongoose, Validator

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {GraphQLModule} from "@nestjs/graphql";
+import {join} from 'path';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      debug: false,
+      cors: true,
+      introspection: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
